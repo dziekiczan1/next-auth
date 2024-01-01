36660f9d60d9df3def0826f491782ffd576494d7
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-01-01 18:18:34 +0100

    add login schema

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+"use client";
+
+import * as z from "zod";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LoginSchema } from "@/schemas";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 import { CardWrapper } from "./card-wrapper";
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
   return (
     <CardWrapper
       headerLabel="Welcome back"
