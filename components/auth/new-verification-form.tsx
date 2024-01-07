6c4e6c717037610a44312d0ee39f7dd34e3e1033
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-01-07 18:37:50 +0100

    install react-spinners and add new verification action

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,5 +1,35 @@
 "use client";
 
+import { useCallback, useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
+
+import { newVerification } from "@/actions/new-verification";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+
 export const NewVerificationForm = () => {
-  return <div>New verification form</div>;
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (!token) return;
+
+    newVerification(token);
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex items-center w-full justify-center">
+        <BeatLoader />
+      </div>
+    </CardWrapper>
+  );
 };
