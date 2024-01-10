961eb1dea7bdef4fe34fc7c5706fabd581cc4e1b
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-01-10 20:27:06 +0100

    add server page and user info component

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -1,9 +1,10 @@
 import { currentUser } from "@/lib/auth";
+import { UserInfo } from "@/components/user-info";
 
 const ServerPage = async () => {
   const user = await currentUser();
 
-  return <div>Server Page</div>;
+  return <UserInfo label="💻 Server component" user={user} />;
 };
 
 export default ServerPage;
