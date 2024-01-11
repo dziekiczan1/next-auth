dd4f643e7980d81b61c46e108ce3619d2aff9abc
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-01-11 20:31:07 +0100

    add role gate component

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,9 +1,34 @@
-"use client";
+import { RoleGate } from "@/components/auth/role-gate";
+import { FormSuccess } from "@/components/form-success";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { currentRole } from "@/lib/auth";
+import { UserRole } from "@prisma/client";
 
-import React from "react";
+const AdminPage = async () => {
+  const role = await currentRole();
 
-const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">🔑 Admin</p>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message="You are allowed to see this content!" />
+        </RoleGate>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-only API Route</p>
+          <Button onClick={() => {}}>Click to test</Button>
+        </div>
+
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-only Server Action</p>
+          <Button onClick={() => {}}>Click to test</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
