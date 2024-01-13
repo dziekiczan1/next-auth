ad3783e77e3b13eb2f7483c94f9d2558636b4679
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-01-13 09:53:08 +0100

    add switch and select from shadcn

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,15 +1,39 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useTransition } from "react";
 import { useSession } from "next-auth/react";
 
+import { SettingsSchema } from "@/schemas";
 import { settings } from "@/actions/settings";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormLabel,
+  FormDescription,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+import { UserRole } from "@prisma/client";
 
 const SettingsPage = () => {
   const { update } = useSession();
   const [isPending, setTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
 
   const onClick = () => {
     setTransition(() => {
@@ -17,6 +41,7 @@ const SettingsPage = () => {
         name: "test",
       }).then(() => {
         update();
+        56;
       });
     });
   };
